fb6102194b51222984cc3235b316ff69d7e7c609
jorejia <lijia12@lzu.edu.cn>
2021-03-22 21:31:06 +0800

    update

--- SCRIPTS/X86/02_target_only.sh
+++ SCRIPTS/X86/02_target_only.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-#翻译及部分功能优化
+
+# 使用 O2 级别的优化
+sed -i 's/O3/O2/g' include/target.mk
+
+# 翻译及部分功能优化
 cp -rf ../PATCH/duplicate/addition-trans-zh ./package/lean/lean-translate
-sed -i '/openssl/d' ./package/lean/lean-translate/files/zzz-default-settings
-sed -i '/banirq/d' ./package/lean/lean-translate/files/zzz-default-settings
-sed -i '/rngd/d' ./package/lean/lean-translate/files/zzz-default-settings
+
+# 在 X86 架构下移除 Shadowsocks-rust
+sed -i '/Rust:/d' package/lean/luci-app-ssr-plus/Makefile
+sed -i '/Rust:/d' package/new/luci-app-passwall/Makefile
+sed -i '/Rust:/d' package/lean/luci-app-vssr/Makefile
 
 <<'COMMENT'
 #Vermagic
@@ -13,13 +19,14 @@ zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > .v
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 COMMENT
 
-#Vermagic 2102 SNAPSHOT ONLY
+# 对齐内核 Vermagic
 wget https://downloads.openwrt.org/releases/21.02-SNAPSHOT/targets/x86/64/packages/Packages.gz
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > .vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 
-#预配置一些插件
+# 预配置一些插件
 cp -rf ../PATCH/X86/files ./files
 
+chmod -R 755 ./
 
 exit 0
