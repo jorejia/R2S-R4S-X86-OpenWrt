7cdc5cecc38549925bbabe997e057177ce51cf21
jorejia <lijia12@lzu.edu.cn>
2021-03-23 16:01:41 +0800

    Update 02_target_only.sh

--- SCRIPTS/R2S/02_target_only.sh
+++ SCRIPTS/R2S/02_target_only.sh
@@ -44,6 +44,9 @@ cp -f ../PATCH/new/main/998-rockchip-enable-i2c0-on-NanoPi-R2S.patch ./target/li
 patch -p1 < ../PATCH/new/main/0002-IRQ-and-disable-eth0-tcp-udp-offloading-tx-rx.patch
 COMMENT
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 # 交换 lan/wan 口
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
